0824eb1fbcf914bb94aef05862fdcfde129f7bb6
Unknown <picarddamien@outlook.com>
2018-03-08 15:18:36 +0100

    refonte graphique

--- ressource/js/script.js
+++ ressource/js/script.js
@@ -8,4 +8,20 @@ $(document).ready(function(){
       $("nav").removeClass("sticky");
     }
   })
+
+  $(".js-scroll-artiste").click(function() {
+  $("html, body").animate({scrollTop: $(".js-section-artiste").offset().top}, 2000);
+  })
+
+  $(".js-scroll-contact").click(function() {
+    $("html, body").animate({scrollTop: $(".js-section-contact").offset().top}, 2000);
+  })
+
+  $(".js-scroll-dessins").click(function() {
+    $("html, body").animate({scrollTop: $(".js-section-dessins").offset().top}, 2000);
+  })
+
+  $(".js-scroll-adresses").click(function() {
+    $("html, body").animate({scrollTop: $(".js-section-adresses").offset().top}, 2000);
+  })
 });
